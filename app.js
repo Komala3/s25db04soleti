e0b160413c1ed6246265644de92239c9ba865e38
Komala3 <S579104@nwmissouri.edu>
2025-04-09 17:33:33 -0500

    new changes

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
@@ -6,26 +6,13 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 
-// MongoDB Connection Setup
-const connectionString = process.env.MONGO_CON;
-mongoose = require('mongoose');
-mongoose.connect(connectionString);
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.on('connecting', () => console.log('Connecting to MongoDB...'));
-db.on('connected', () => console.log('MongoDB connected!'))
-
-db.once("open", function(){
-console.log("Connection to DB succeeded")});
-// Import routers
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dronesRouter = require('./routes/drones');
 var pickRouter = require('./routes/pick');
 var resourceRouter = require('./routes/resource');
 const drones = require("./models/drones");
-const constants = require('constants');
+
 
 const app = express();
 
@@ -40,29 +27,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/drones', dronesRouter);
-app.use('/pick', pickRouter);
-app.use('/resource', resourceRouter);
-app.get('/drones', require('./controllers/drones').drones_list);
+require('dotenv').config();
+// MongoDB Connection Setup
+const connectionString = process.env.MONGO_CON;
+mongoose = require('mongoose');
+mongoose.connect(connectionString);
 
 
-// Database seeding function
-async function seedDatabase() {
-  try {
-    await Drone.deleteMany();
-    await Drone.create([
-      { model: "Phantom 4", brand: "DJI", range: 6000 },
-      { model: "Anafi", brand: "Parrot", range: 4000 },
-      { model: "Evo II", brand: "Autel", range: 9000 }
-    ]);
-    console.log('Database seeded successfully');
-  } catch (err) {
-    console.error('Error seeding database:', err);
-  }
-}
+
+
+
 
 // We can seed the collection if needed on server start
 async function recreateDB(){
@@ -71,7 +45,7 @@ async function recreateDB(){
  let instance1 = new drones({model:"Phantom 4", brand: "DJI", range:6000});
  instance1.save().then(doc=>{
  console.log("First object saved")}).catch(err=>{
-  console.error(err)});
+console.error(err)});
 
  let instance2 = new drones({model:"Anafi", brand: "Parrot", range:4000});
  instance2.save().then(doc=>{
@@ -87,4 +61,47 @@ async function recreateDB(){
 let reseed = true;
 if (reseed) recreateDB();
 
+// Routes
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/drones', dronesRouter);
+app.use('/pick', pickRouter);
+app.use('/resource', resourceRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+//Get the default connection
+var db = mongoose.connection;
+//Bind connection to error event
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once("open", function(){
+console.log("Connection to DB succeeded")});
+
 module.exports = app;
+
+
+
+
+
+
+
+
+db.once("open", function(){
+  console.log("Connection to DB succeeded")});
+  // Import routers
+
+  app.get('/drones', require('./controllers/drones').drones_list);
