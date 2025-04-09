5731f1788101fb0384411b9ba998c1fd1eeb5895
Komala3 <S579104@nwmissouri.edu>
2025-04-09 10:23:05 -0500

    changes

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const Drone = require('./models/drones');
 var drones = require("./models/drones");
 
 // MongoDB Connection Setup
